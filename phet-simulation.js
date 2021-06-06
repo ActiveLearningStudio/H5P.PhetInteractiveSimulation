9dcb3ca1f28c636dce8a6c4e8ec669fbfebb9974
i-do-dev <i.do.dev@outlook.com>
2021-06-06 09:48:42 +0500

    trigger resize while rendering by defnining event

--- phet-simulation.js
+++ phet-simulation.js
@@ -3,14 +3,27 @@ var H5P = H5P || {};
 H5P.PhetInteractiveSimulation = (function($) {
     
     function PhetInteractiveSimulation(options, id) {
+        var self = this;
         this.id = id;
         this.options = options;
+        
+        self.on('resize', function () {
+            if (this.container) {
+                let width = H5P.jQuery(this.container).width();
+                let height = width * (9/16);
+                $('#phetiframe').width(width);
+                $('#phetiframe').height(height);
+            }
+        });
     }
 
     PhetInteractiveSimulation.prototype.attach = function($container) {
+        this.container = $container;
         var fileUrl = window.location.origin + "/sites/default/files/h5p/content/" + this.id + "/files/" + this.options.phetsimulation.path.split('/')[parseInt(this.options.phetsimulation.path.split('/').length - 1)];
-        $container.append('<iframe src="' + fileUrl + '"></iframe>');
+        $container.append('<iframe id="phetiframe" src="' + fileUrl + '"></iframe>');
+        this.trigger('resize');
     }
 
     return PhetInteractiveSimulation;
+
 })(H5P.jQuery);
